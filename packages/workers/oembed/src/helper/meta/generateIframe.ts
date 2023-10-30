b48a81b80ca0aaa7eacdec2f1d7f23b38998b0ec
abundant155 <129884342+abundant155@users.noreply.github.com>
2023-10-30 03:59:20 +0000

    Pending changes exported from your codespace

--- packages/workers/oembed/src/helper/meta/generateIframe.ts
+++ packages/workers/oembed/src/helper/meta/generateIframe.ts
@@ -22,7 +22,7 @@ const youtubeRegex =
   /^https?:\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w-]+)(?:\?.*)?$/;
 const lenstubeRegex =
   /^https?:\/\/lenstube\.xyz\/watch\/[\dA-Za-z-]+(\?si=[\dA-Za-z]+)?$/;
-const livepeerRegex = 
+const livepeerUrlRegex = 
   /^https?:\/\/lvpr\.tv\/\?v=[0-9a-z]{16}$/;
 
 const generateIframe = (
@@ -78,7 +78,7 @@ const generateIframe = (
 
       return null;
     case 'lvpr.tv':
-      if (livepeerRegex.test(url)) {
+      if (livepeerUrlRegex.test(url)) {
         return `<iframe src="${pickedUrl}" ${universalSize}></iframe>`;
       }
       return null;
